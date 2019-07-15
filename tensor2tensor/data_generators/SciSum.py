9fce072cf8e176b11d250af60bc2ed6d9697c6d8
Santosh Gupta <Santosh.gupta.eng@gmail.com>
2019-07-15 09:06:22 -0700

    Update SciSum.py

--- tensor2tensor/data_generators/SciSum.py
+++ tensor2tensor/data_generators/SciSum.py
@@ -36,4 +36,131 @@ END_TOKENS = [
 if not os.path.exists('InitialData'):
         os.makedirs('InitialData')
         
+
+def download_file_from_google_drive(id, destination):
+    URL = "https://docs.google.com/uc?export=download"
+
+    session = requests.Session()
+
+    response = session.get(URL, params = { 'id' : id }, stream = True)
+    token = get_confirm_token(response)
+
+    if token:
+        params = { 'id' : id, 'confirm' : token }
+        response = session.get(URL, params = params, stream = True)
+
+    save_response_content(response, destination)    
+
+def get_confirm_token(response):
+    for key, value in response.cookies.items():
+        if key.startswith('download_warning'):
+            return value
+
+    return None
+
+def save_response_content(response, destination):
+    CHUNK_SIZE = 32768
+
+    with open(destination, "wb") as f:
+        for chunk in response.iter_content(CHUNK_SIZE):
+            if chunk: # filter out keep-alive new chunks
+                f.write(chunk)
+                
+download_file_from_google_drive('1-8UndAYFPYRUmb_y6NUvN6KSNpc2vvEc', 'InitialData/sampledata.parquet.gz')
+                
+summ_finalpath =  'InitialData/'
+
+all_files = tf.gfile.Glob(summ_finalpath + "*")
+                
+all_files = all_files[0:1]
+                
+dftest = pd.read_parquet( all_files[0] )    
+                
+def example_generator(all_files, sum_token):
+    # Generate examples
+
+    story_summary_split_token = u" <summary> " if sum_token else " "
+    
+    for file in all_files:
+        print(file)
+        dataframe = pd.read_parquet( file , engine = 'fastparquet') 
+        for i in range(dataframe.shape[0]):
+            summary = dataframe['Summary'].iloc[i]
+            source = dataframe['paperSection'].iloc[i]
+            yield source + ' ' + story_summary_split_token + ' ' + summary
+                
+def _story_summary_split(story):
+    split_str = u" <summary> "
+    split_str_len = len(split_str)
+    split_pos = story.find(split_str)
+    return story[:split_pos], story[split_pos + split_str_len:]  # story, summary    
+                
+                
+def write_raw_text_to_files(all_files, dataset_split, tmp_dir):
+
+    def write_to_file(all_files, tmp_dir, filename):
+    #Write text to files
+        with io.open( os.path.join(tmp_dir, filename + ".source"), "w", encoding="utf-8") as fsource:
+            with io.open( os.path.join(tmp_dir, filename + ".target"), "w", encoding="utf-8") as fsummary:
+                for example in example_generator(all_files, sum_token=True):
+                    source, summary = _story_summary_split(example)
+                    fsource.write(source + "\n")
+                    fsummary.write(summary + "\n")
+
+    if dataset_split == problem.DatasetSplit.TRAIN:
+        filename = "summaryData.train"
+    elif dataset_split == problem.DatasetSplit.EVAL:
+        filename = "summaryData.dev"
+    else:
+        filename = "summaryData.test"
+
+    tf.logging.info("Writing %s" % filename)
+    write_to_file(all_files, tmp_dir, filename)
+                
+                
+@registry.register_problem
+class SummarizeScientificSections65k(text_problems.Text2TextProblem):
+  #Summarize CNN and Daily Mail articles to their summary highlights.
+
+    def generate_text_for_vocab(self, data_dir, tmp_dir):
+        del data_dir
+#         all_files, urls_path = _maybe_download_corpora(tmp_dir,
+#                                                        problem.DatasetSplit.TRAIN)
+        return example_generator(all_files, sum_token=False)
+
+    @property
+    def dataset_splits(self):
+    #Splits of data to produce and number of output shards for each."""
+        return [{
+            "split": problem.DatasetSplit.TRAIN,
+            "shards": 100,
+        }, {
+            "split": problem.DatasetSplit.EVAL,
+            "shards": 10,
+        }, {
+            "split": problem.DatasetSplit.TEST,
+            "shards": 10,
+        }]
+
+    def is_generate_per_split(self):
+        return True
+
+    def generate_samples(self, data_dir, tmp_dir, dataset_split):
+        del data_dir
+#         all_files, urls_path = _maybe_download_corpora(tmp_dir, dataset_split)
+        write_raw_text_to_files(all_files, dataset_split, tmp_dir)
+        for example in example_generator(all_files, sum_token=True):
+            story, summary = _story_summary_split(example)
+            yield {"inputs": story, "targets": summary}
         
+    @property
+    def approx_vocab_size(self):
+    #Approximate vocab size to generate. Only for VocabType.SUBWORD."""
+        return 2**16  # ~32k
+    #Santosh Edit: changed from 2**15 to 2**16
+                
+                
+                
+                
+                
+                
