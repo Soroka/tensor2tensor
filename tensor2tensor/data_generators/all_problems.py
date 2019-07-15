572b7c3af8fce035f1c756908e439bf6852c96cb
Santosh Gupta <Santosh.gupta.eng@gmail.com>
2019-07-15 10:52:43 -0700

    Update all_problems.py

--- tensor2tensor/data_generators/all_problems.py
+++ tensor2tensor/data_generators/all_problems.py
@@ -68,6 +68,7 @@ MODULES = [
     "tensor2tensor.data_generators.quora_qpairs",
     "tensor2tensor.data_generators.rte",
     "tensor2tensor.data_generators.sci_sum",
+    "tensor2tensor.data_generators.sci_sum_gdrive",
     "tensor2tensor.data_generators.scitail",
     "tensor2tensor.data_generators.snli",
     "tensor2tensor.data_generators.stanford_nli",
